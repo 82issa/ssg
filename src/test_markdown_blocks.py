438f86967e574a71f9817c37502edab41c7d3c69
Nadim Issa <82issa@gmail.com>
2025-04-14 21:21:10 -0400

    f

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -6,6 +6,7 @@ from markdown_blocks import (
     BlockType,
 )
 
+
 class TestMarkdownToHTML(unittest.TestCase):
     def test_markdown_to_blocks(self):
         md = """
@@ -64,20 +65,20 @@ This is the same paragraph on a new line
         block = "paragraph"
         self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
-        def test_paragraph(self):
-            md = """
+    def test_paragraph(self):
+        md = """
 This is **bolded** paragraph
 text in a p
 tag here
 
 """
 
-            node = markdown_to_html_node(md)
-            html = node.to_html()
-            self.assertEqual(
-                html,
-                "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p></div>",
-            )
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p></div>",
+        )
 
     def test_paragraphs(self):
         md = """
@@ -147,7 +148,7 @@ this is paragraph text
             "<div><blockquote>This is a blockquote block</blockquote><p>this is paragraph text</p></div>",
         )
 
-    def test_code(self):
+    def test_codeblock(self):
         md = """
 ```
 This is text that _should_ remain
@@ -162,6 +163,6 @@ the **same** even with inline stuff
             "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
         )
 
+
 if __name__ == "__main__":
     unittest.main()
-
